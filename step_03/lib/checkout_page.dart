21e9e4651b715cb35d9ae12479bcb7f1cbaf3200
Ashish-Raturi <ashishraturi368@gmail.com>
2022-05-10 13:05:10 +0530

    code_updated

---
+++ step_03/lib/checkout_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CheckoutPage extends StatefulWidget {
+  final String url;
+  const CheckoutPage({Key? key, required this.url}) : super(key: key);
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+}
+
+class _CheckoutPageState extends State<CheckoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: WebView(
+            initialUrl: widget.url,
+            javascriptMode: JavascriptMode.unrestricted,
+            navigationDelegate: (NavigationRequest request) {
+              if (request.url.startsWith('https://success.com')) {
+                Navigator.of(context).pop('success'); // <-- Handle success case
+              } else if (request.url.startsWith('https://cancel.com')) {
+                Navigator.of(context).pop('cancel'); // <-- Handle cancel case
+              }
+              return NavigationDecision.navigate;
+            }),
+      ),
+    );
+  }
+}
---
+++ step_04/lib/color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color c1 = const Color(0xff8f6fff);
+Color c2 = const Color(0xfff6f2ff);
