28ec0d178b596065d6368d8434f78d9498c847e5
Ashish-Raturi <ashishraturi368@gmail.com>
2022-05-13 00:22:39 +0530

    code_updated

--- step_01/lib/main.dart
+++ step_01/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:stripe/home_page.dart';
 import 'package:stripe/signin_page.dart';
 
 void main() async {
--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stripe/home_page.dart';
+import 'package:home_page.dart';
 
 void main() {
   runApp(const MyApp());
