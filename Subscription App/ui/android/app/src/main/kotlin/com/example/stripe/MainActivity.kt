33f403836baf04e3d5b90cddcbe2f2d43b01bca5
Ashish-Raturi <ashishraturi368@gmail.com>
2022-05-20 12:51:55 +0530

    code_updated

--- Subscription App/ui/android/app/src/main/kotlin/com/example/stripe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stripe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Subscription App/ui/lib/color.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color c1 = const Color(0xff8f6fff);
-Color c2 = const Color(0xfff6f2ff);
